b9f4efa103844c222daad0daeb1958e3ce065043
Harsh Vardhan <54477764+harsh-vardhan152@users.noreply.github.com>
2025-04-21 23:39:01 +0530

    Initial commit

---
+++ src/test/java/com/harshvardhan/quality_app/QualityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshvardhan.quality_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
