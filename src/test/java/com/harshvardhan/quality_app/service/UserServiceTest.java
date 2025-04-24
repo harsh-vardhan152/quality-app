0a580a391f931836ceff1202d0f2c27a850f5e17
Harsh Vardhan <hv49271@gmail.com>
2025-04-24 19:54:35 +0530

    Add Mockito dependency and initial UserServiceTest class
    Added Mockito as a test dependency in `pom.xml` to support unit testing. Created an initial `UserServiceTest` class with a setup method and a placeholder test case for user creation.

---
+++ src/test/java/com/harshvardhan/quality_app/service/UserServiceTest.java
@@ -0,0 +1,26 @@
+package com.harshvardhan.quality_app.service;
+
+import com.harshvardhan.quality_app.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+
+    private User testUser;
+
+    @BeforeEach
+    void StepUp() {
+        testUser = new User();
+        testUser.setId(1L);
+        testUser.setName("Test");
+        testUser.setEmail("test@gmail.com");
+        testUser.setPassword("Test");
+    }
+
+    @Test
+    void createUser_Success() {
+        // TODO document why this method is empty
+    }
+
+}
